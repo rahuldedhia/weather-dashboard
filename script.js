928ee96ceada25a18f642db21b2d5d7233a2bec0
RCoder <rahul.dedhia@gmail.com>
2020-04-11 15:20:07 -0500

    Fixed the serach box and navigation click

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var savedCity = [""];
-var addCity;
-loadCitySummary("Atlanta");
-//loadNav();
-var searchedCity;
 var citySearchForm = document.querySelector("#citysearch-form");
+var citySearchIcon = document.querySelector("#searchicon");
+var savedCity = [];
+var searchedCity;
+var addCity;
+var savedCities = JSON.parse(localStorage.getItem("cityNames"));
+
+if (savedCities != null) {
+  loadCitySummary(savedCities[savedCities.length - 1]);
+}
 
 function loadCitySummary(City) {
   var queryURL = "http://api.openweathermap.org/data/2.5/find?q=" + City + "&units=imperial&appid=53469888c4f263afbeff1527801e0c67";
@@ -12,7 +16,6 @@ function loadCitySummary(City) {
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    //console.log(response);
     var cityTitle = $("<h3><strong>").text(response.list[0].name + " (" + moment().format("MM/DD/YYYY") + ")");
     var citySummary = $("<br><p>").text("Temperature: " + Math.round(response.list[0].main.temp) + " °F");
     var humidity = $("<p>").text("Humidity: " + Math.round(response.list[0].main.humidity) + "%");
@@ -60,34 +63,34 @@ citySearchForm.addEventListener("submit", function (event) {
   loadCitySummary(searchedCity);
 });
 
+citySearchForm.addEventListener("click", function () {
+  event.preventDefault();
+  searchedCity = $("#citySearch").val().trim();
+  loadCitySummary(searchedCity);
+});
+
 function loadNav() {
-  //console.log(JSON.parse(localStorage.getItem("cityNames")));
-   var savedCity1 = JSON.parse(localStorage.getItem("cityNames"));
-   //console.log(savedCity1);
-     if (savedCity1 != null) {
-      //console.log(JSON.parse(localStorage.getItem("cityNames")));
-      savedCity = JSON.parse(localStorage.getItem("cityNames"));
-     }
-     savedCity.push(addCity); 
-    //console.log(savedCity);
-    removeDupsAndSave(savedCity);
+  var savedCitytemp = JSON.parse(localStorage.getItem("cityNames"));
+  if (savedCitytemp != null) {
     savedCity = JSON.parse(localStorage.getItem("cityNames"));
-    // console.log(savedCity);
-    //console.log(JSON.parse(localStorage.getItem("cityNames")));
+  }
+  savedCity.push(addCity);
+  removeDupsAndSave(savedCity);
+  savedCity = JSON.parse(localStorage.getItem("cityNames"));
   $("#cityList").empty();
-  
+
   if (savedCity != null) {
-  for (var i = 1; i < savedCity.length; i++) {
-    var navCity = $("<li>");
-    navCity.addClass("navbox");
-    navCity.append('<button class="btn btn-light btn-block text-left p-3" value="' +savedCity[i] + '">' + savedCity[i] + "</button>");
-    $("#cityList").append(navCity);
-  } 
-}
+    for (var i = 0; i < savedCity.length; i++) {
+      var navCity = $("<li>");
+      navCity.addClass("navbox");
+      navCity.append('<button class="btn btn-light btn-block text-left p-2" value="' + savedCity[i] + '">' + savedCity[i] + "</button>");
+      $("#cityList").append(navCity);
+    }
+  }
   navClick();
 }
 
-function navClick () {
+function navClick() {
   $("button").on("click", function () {
     loadCitySummary($(this).val());
   });
@@ -96,19 +99,15 @@ function navClick () {
 function removeDupsAndSave(array) {
   var newArray = [];
   for (i = 0; i < array.length; i++) {
-      if (!newArray.includes(array[i])) {
-          newArray.push(array[i]);
-     }
+    if (!newArray.includes(array[i])) {
+      newArray.push(array[i]);
+    }
   }
-  //console.log(newArray);
   localStorage.setItem("cityNames", JSON.stringify(newArray));
 }
 
 function clearLS() {
   localStorage.removeItem("cityNames");
 }
-
-//loadCitySummary(searchedCity);
-//loadNav();
 
 //clearLS();
