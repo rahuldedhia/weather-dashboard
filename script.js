b8f4fecaabb1c9e270820b143f37b0fd758d3f3c
RCoder <rahul.dedhia@gmail.com>
2020-04-14 19:39:35 -0500

    cleaned up code and added comments

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var citySearchForm = document.querySelector("#citysearch-form");
-var citySearchIcon = document.querySelector("#searchicon");
 var savedCity = [];
 var searchedCity;
 var addCity;
-var savedCities = JSON.parse(localStorage.getItem("cityNames"));
+var lastCity = localStorage.getItem("lastCity");
 
-if (savedCities != null) {
-  loadCitySummary(savedCities[savedCities.length - 1]);
+//retrieves last city that was looked at
+if (lastCity != null) {
+  loadCitySummary(lastCity);
 }
 
+//Main Function which calls the curretnweather API to retrieve summary data
 function loadCitySummary(City) {
   var queryURL = "https://api.openweathermap.org/data/2.5/find?q=" + City + "&units=imperial&appid=53469888c4f263afbeff1527801e0c67";
 
@@ -16,23 +16,26 @@ function loadCitySummary(City) {
     url: queryURL,
     method: "POST",
   }).then(function (response) {
+    var lat = response.list[0].coord.lat;
+    var lon = response.list[0].coord.lon;
+    load5Day(lat, lon);
+
     var cityTitle = $("<h3><strong>").text(response.list[0].name + " (" + moment().format("MM/DD/YYYY") + ")");
     var citySummary = $("<br><p>").text("Temperature: " + Math.round(response.list[0].main.temp) + " °F");
     var humidity = $("<p>").text("Humidity: " + Math.round(response.list[0].main.humidity) + "%");
     var windSpeed = $("<p>").text("Wind Speed: " + Math.round(response.list[0].wind.speed) + " MPH");
-    var lat = response.list[0].coord.lat;
-    var lon = response.list[0].coord.lon;
-    load5Day(lat, lon);
     $(cityTitle).append('<img src="https://openweathermap.org/img/wn/' + response.list[0].weather[0].icon + '.png" />');
 
     $("#cityWeatherSummary").empty();
     $("#cityWeatherSummary").append(cityTitle, citySummary, humidity, windSpeed);
 
     addCity = response.list[0].name;
+    localStorage.setItem("lastCity", addCity);
     loadNav();
   });
 }
 
+//function for 5 day weather details using OneCall API
 function load5Day(lat, lon) {
   var query5dURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=53469888c4f263afbeff1527801e0c67';
   
@@ -57,21 +60,23 @@ function load5Day(lat, lon) {
   navClick();
 }
 
-citySearchForm.addEventListener("submit", function (event) {
+//listener looking if any city is searched for using the search button
+$("#citysearch-form").on("submit", function (event) {
   event.preventDefault();
   searchedCity = $("#citySearch").val().trim();
   loadCitySummary(searchedCity);
 });
 
-citySearchForm.addEventListener("click", function () {
+//listener looking if any city is searched by hitting the enter button on keyboard
+$("#citysearch-form").on("click", function () {  
   event.preventDefault();
   searchedCity = $("#citySearch").val().trim();
   loadCitySummary(searchedCity);
 });
 
+//function that loads the previously searched cities
 function loadNav() {
-  var savedCitytemp = JSON.parse(localStorage.getItem("cityNames"));
-  if (savedCitytemp != null) {
+  if (JSON.parse(localStorage.getItem("cityNames")) != null) {
     savedCity = JSON.parse(localStorage.getItem("cityNames"));
   }
   savedCity.push(addCity);
@@ -90,12 +95,14 @@ function loadNav() {
   navClick();
 }
 
+//function to load the city that is clicked on from the left navigation
 function navClick() {
   $("button").on("click", function () {
     loadCitySummary($(this).val());
   });
 }
 
+//This function saves only unique cities in the local storage that are searched for using the search box so that they can be loaded when the users returns next time 
 function removeDupsAndSave(array) {
   var newArray = [];
   for (i = 0; i < array.length; i++) {
@@ -106,8 +113,10 @@ function removeDupsAndSave(array) {
   localStorage.setItem("cityNames", JSON.stringify(newArray));
 }
 
+//This function is used to clear memory items. This function is not being called from anywhere currently and is avialable to use if required
 function clearLS() {
   localStorage.removeItem("cityNames");
+  localStorage.removeItem("lastCity");
 }
 
 //clearLS();
